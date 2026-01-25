9eea56082cf2660cfca72955f0593d7abdcfc77b
Jeongwon Lim <skygarden100@gmail.com>
2026-01-25 17:57:21 -0500

    Add initial Spring Boot backend source code

---
+++ src/test/java/com/jeongwonlim/springbootbackendci/SpringbootbackendciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeongwonlim.springbootbackendci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbackendciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
